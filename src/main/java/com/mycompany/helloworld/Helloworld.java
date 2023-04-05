03cbff7640dff377397b705985b2ebbee552c0b4
AlphaTech1 <tjscgng26@gmail.com>
2023-04-05 23:31:22 +0800

    enlo

---
+++ src/main/java/com/mycompany/helloworld/Helloworld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author JOHN
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
